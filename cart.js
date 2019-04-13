2a555bdd3a1fec6ed16dc58c8a6730b09e8261e0
Andrdragon] <andrdragon@gmail.com>
2019-04-13 13:49:05 +0300

    commit

---
+++ cart.js
@@ -0,0 +1,24 @@
+var goods = [
+{
+	name: "Яблоки",
+	picture: "https://static.agcanada.com/wp-content/uploads/sites/5/2018/11/apple_GettyImages186843005_cmyk.jpg"
+	cost: 10,
+	count: 23,
+	identity: "apple"
+}
+{
+	name: "Персики",
+	picture: "https://freshmart.com.ua/images/232/mainimg/de630a66e0117978987ce515f7265a9a_big.jpeg"
+	cost: 15,
+	count: 27,
+	identity: "peach"
+}
+{
+	name: "Груши",
+	picture: "https://freshmart.com.ua/images/455/mainimg/3555749e25199caf73802ab26a0c2ed7_big.jpeg"
+	cost: 20,
+	count: 13,
+	identity: "pear"
+}
+
+];
